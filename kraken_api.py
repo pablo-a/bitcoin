74dcab7dcc3f0d7ea092bf7f1fb87b6429b58a19
pablo-a <pabril@student.42.fr>
2017-06-16 19:05:21 +0200

    add some queries to api. need to think about structure.

--- kraken_api.py
+++ kraken_api.py
@@ -23,6 +23,16 @@ import time
 class KrakenApi(object):
     """docstring de la classe KrakenApi : """
 
+    def check_asset_exists(self, asset_ticker):
+        """INPUT : asset pair Ticker
+        OUTPUT : 1 if it exists, 0 otherwise"""
+        uri = "https://api.kraken.com/0/public/AssetPairs"
+        params = {"pair" : asset_ticker}
+        response = requests.get(uri, params=params)
+        data = json.loads(response.content)
+
+        return 0 if data['error'] else 1
+
     def get_server_time(self):
         """response time is approximative and supposed to be UTC. (-1h. careful with changement d'heure!)
         exemple response :
@@ -41,25 +51,56 @@ class KrakenApi(object):
 
         return (data['result']["unixtime"], date_object)
 
-    def get_asset_value(self, asset_ticker):
-        """INPUT : One or more asset ticker (ex : XETHZUSD[,XBTCZUSD,...])
+    def get_asset_info(self, asset_ticker):
+        """INPUT : One or more asset ticker (ex : XETHZUSD[,XBTCZEUR,...])
         OUTPUT : list of asset object infos.
-        a = ask array(<price>, <whole lot volume>, <lot volume>),
-        b = bid array(<price>, <whole lot volume>, <lot volume>),
-        c = last trade closed array(<price>, <lot volume>),
-        v = volume array(<today>, <last 24 hours>),
-        p = volume weighted average price array(<today>, <last 24 hours>),
-        t = number of trades array(<today>, <last 24 hours>),
-        l = low array(<today>, <last 24 hours>),
-        h = high array(<today>, <last 24 hours>),
-        o = today's opening price"""
+        <pair_name> = pair name
+            a = ask array(<price>, <whole lot volume>, <lot volume>),
+            b = bid array(<price>, <whole lot volume>, <lot volume>),
+            c = last trade closed array(<price>, <lot volume>),
+            v = volume array(<today>, <last 24 hours>),
+            p = volume weighted average price array(<today>, <last 24 hours>),
+            t = number of trades array(<today>, <last 24 hours>),
+            l = low array(<today>, <last 24 hours>),
+            h = high array(<today>, <last 24 hours>),
+            o = today's opening price"""
 
         uri = "https://api.kraken.com/0/public/Ticker"
         params = {"pair" : asset_ticker}
-
         response = requests.get(uri, params=params)
         data = json.loads(response.content)
-        return [asset for asset in data["result"]]
+
+        if data['error']:
+            print(data['error'])
+            return None
+
+        return [data['result'][asset] for asset in data["result"]]
+
+    def get_asset_value(self, asset_ticker, interval=1, since=""):
+        """INPUT :
+        asset_ticker : asset pair to get OHLC data for (can be a comma separated list)
+        interval : time frame interval in minutes (optional):
+        1 (default), 5, 15, 30, 60, 240, 1440, 10080, 21600
+        since = return committed OHLC data since given timestamp (optional.  exclusive)
+        OUTPUT : list of asset values
+        <pair_name> = pair name
+            array of array entries(<time>, <open>, <high>, <low>, <close>, <vwap>, <volume>, <count>)
+        last = id to be used as since when polling for new, committed OHLC data"""
+
+        allowed_interval = [1, 5, 15, 30, 60, 240, 1440, 10080, 21160]
+        if interval not in allowed_interval:
+            print("Wrong interval value")
+            return None
+
+        uri = "https://api.kraken.com/0/public/OHLC"
+        params = {"pair" : asset_ticker, "interval" : interval, "since" : since}
+        response = requests(uri, params)
+        data = json.loads(response.content)
+        if data['error']:
+            print(data['error'])
+            return None
+
+        return [data['result'][asset] for asset in data['result']]
 
 
 if __name__ == '__main__':
@@ -67,11 +108,9 @@ if __name__ == '__main__':
     thing = api.get_server_time()
     print(thing)
 
-    asset_ticker = "XETHZUSD"
-    values = api.get_asset_value(asset_ticker=)
-    print(values)
+    asset_ticker = "XETHZUSD,XBTCZUSD"
 
-
-
-
-    
+    while 1:
+        print(api.check_asset_exists("XETHZUSD"))
+        values = api.get_asset_info(asset_ticker)
+        print(values)
